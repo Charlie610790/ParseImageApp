3fe71ec1411131f275c2b9b88a1788483600f7ef
Charlie Jones <myupstateproperties@gmail.com>
2014-06-17 21:45:45 -0400

    add fixed position header and image add window

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,3 +29,39 @@ collection.fetch().done(function () {
 		
 	})
 })
+
+
+//action that happens when the submit button is clicked
+$('.addButton').click(function() {
+    //takse the input value of the message field and assigns it to the var message
+    var message = $('.input').val();
+    //takes the value of the message field and clears it
+    $('.input').val('');
+    //takes the time and sets it at the var time
+    var time = timeRightNow;
+    //creates a new instance of the constructor POST passing the three assigned variables
+    var outgoingPost = new post(url, comment);
+
+    updateChat(outgoingPost);
+
+});
+
+var Post = Parse.Object.extend("Post");
+var post = new Post();
+var message = $('.input').val();
+
+post.set("URL", 1337);
+post.set("playerName", "Sean Plott");
+post.set("cheatMode", false);
+ 
+gameScore.save(null, {
+  success: function(gameScore) {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + gameScore.id);
+  },
+  error: function(gameScore, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and description.
+    alert('Failed to create new object, with error code: ' + error.description);
+  }
+});
